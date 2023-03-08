5b391a8c2ea94572098cee7baba91733c763d8d9
tangbearrrr <rh.sarawasee@gmail.com>
2023-03-08 21:00:07 +0700

    init

---
+++ index.js
@@ -0,0 +1,23 @@
+const { Client, GatewayIntentBits } = require('discord.js');
+const { token } = require('./config.json');
+
+const client = new Client({
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.MessageContent,
+    ]
+});
+
+client.on('ready', () => {
+    console.log('bot is ready!');
+})
+
+client.on('messageCreate', msg => {
+    if (msg.content === 'ping') {
+        msg.react('✅');
+        msg.channel.send('pong!');
+    }
+})
+
+client.login(token);
